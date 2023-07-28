f5d539f790327e1fc09d69ec02c750fa8e0cb974
hobbs9458 <hobbs9458@gmail.com>
2023-07-28 09:48:24 -0500

    working carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,34 +1,102 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import './Carousel.css';
 
-export default function Carousel({ numSlides = 3 }) {
+export default function Carousel({
+  numSlides = 3,
+  transitionTime = 1500,
+  slideTime = 5000,
+}) {
   const [carouselWidth, setCarouselWidth] = useState(0);
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+  const [isAuto, setIsAuto] = useState(true);
+
   const slidesRef = useRef([]);
   const carouselRef = useRef(0);
   const trackRef = useRef(null);
+  const btnLeftRef = useRef(null);
+  const btnRightRef = useRef(null);
+  const btnFocusRef = useRef(null);
+
+  // const timerRef = useRef(null);
+
+  const totalSlides = numSlides + 2;
 
   const pushRef = (slide) => {
-    if (slide && slidesRef.current.length < numSlides) {
+    if (slide && slidesRef.current.length < totalSlides) {
       slidesRef.current.push(slide);
     }
   };
 
-  const changeSlideLeft = () => {
+  const transition = useCallback(() => {
+    trackRef.current.style.transition = `transform ${
+      transitionTime / 1000
+    }s ease-in-out`;
+  }, [transitionTime]);
+
+  const changeSlideRight = useCallback(
+    (e) => {
+      if (e) {
+        setIsBtnDisabled(true);
+      }
+
+      if (e && isAuto) {
+        setIsAuto(false);
+      }
+
+      if (currentSlide < totalSlides - 1) {
+        transition();
+        setCurrentSlide((prevSlide) => prevSlide + 1);
+      }
+    },
+
+    [currentSlide, totalSlides, transition, isAuto]
+  );
+
+  const changeSlideLeft = (e) => {
+    if (e) {
+      setIsBtnDisabled(true);
+    }
+
+    if (e && isAuto) {
+      setIsAuto(false);
+    }
+
     if (currentSlide > 0) {
+      transition();
       setCurrentSlide((prevSlide) => prevSlide - 1);
     }
   };
 
-  const changeSlideRight = () => {
-    if (currentSlide < numSlides - 1) {
-      setCurrentSlide((prevSlide) => prevSlide + 1);
+  const handleCloneSlides = () => {
+    transition();
+    // clearTimeout(timerRef.current);
+    if (currentSlide === 0) {
+      trackRef.current.style.transition = 'none';
+      setCurrentSlide(totalSlides - 2);
     }
+
+    if (currentSlide === totalSlides - 1) {
+      trackRef.current.style.transition = 'none';
+      setCurrentSlide(1);
+    }
+
+    setIsBtnDisabled(false);
   };
 
-  useEffect(() => {
-    setCarouselWidth(carouselRef.current.offsetWidth);
-  }, [carouselWidth, carouselRef]);
+  const focusBtn = (e) => {
+    if (e.keyCode === 13) {
+      btnFocusRef.current = e.target;
+    } else {
+      btnFocusRef.current = null;
+    }
+  };
 
   useLayoutEffect(() => {
     if (slidesRef.current.length > 0) {
@@ -45,40 +113,96 @@ export default function Carousel({ numSlides = 3 }) {
     trackRef.current.style.transform = `translateX(-${distance})`;
   }, [currentSlide, carouselWidth]);
 
+  useEffect(() => {
+    setCarouselWidth(carouselRef.current.offsetWidth);
+  }, [carouselWidth, carouselRef]);
+
+  useEffect(() => {
+    let id;
+    if (isAuto) {
+      id = setTimeout(
+        () => {
+          // timerRef.current = id;
+          changeSlideRight();
+        },
+        currentSlide === 1 ? slideTime - transitionTime : slideTime
+      );
+    } else {
+      clearTimeout(id);
+    }
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [
+    changeSlideRight,
+    currentSlide,
+    totalSlides,
+    transitionTime,
+    slideTime,
+    isAuto,
+  ]);
+
+  useEffect(() => {
+    if (isBtnDisabled || !btnFocusRef.current) return;
+    btnFocusRef.current.focus();
+    btnFocusRef.current = null;
+  }, [isBtnDisabled]);
+
   return (
     <section ref={carouselRef} className='carousel'>
-      <ul ref={trackRef} className='track'>
+      <ul ref={trackRef} className='track' onTransitionEnd={handleCloneSlides}>
         <li
           ref={pushRef}
-          className='slide slide-0'
+          className='slide slide-0 clone-of-last-slide'
+          style={{ backgroundColor: 'purple' }}
+        >
+          2
+        </li>
+        <li
+          ref={pushRef}
+          className='slide slide-1'
           style={{ backgroundColor: 'lightblue' }}
         >
           0
         </li>
         <li
           ref={pushRef}
-          className='slide slide-1'
+          className='slide slide-2'
           style={{ backgroundColor: 'lightgreen' }}
         >
           1
         </li>
         <li
           ref={pushRef}
-          className='slide slide-2'
-          style={{ backgroundColor: 'lightblue' }}
+          className='slide slide-3'
+          style={{ backgroundColor: 'purple' }}
         >
           2
+        </li>
+        <li
+          ref={pushRef}
+          className='slide slide-4 clone-of-first-slide'
+          style={{ backgroundColor: 'lightblue' }}
+        >
+          0
         </li>
       </ul>
       <button
         className='change-slide-btn change-slide-btn-left'
         onClick={changeSlideLeft}
+        disabled={isBtnDisabled}
+        ref={btnLeftRef}
+        onKeyDown={focusBtn}
       >
         &lt;
       </button>
       <button
         className='change-slide-btn change-slide-btn-right'
         onClick={changeSlideRight}
+        disabled={isBtnDisabled}
+        ref={btnRightRef}
+        onKeyDown={focusBtn}
       >
         &gt;
       </button>
